9229dc0f94fbed33376d532d54d7ba6a2404802b
Aaron Arndt <Aaron Arndt>
2017-10-18 20:02:02 -0500

    Port connected and basic form submission using /add route

--- server1.js
+++ server1.js
@@ -8,6 +8,10 @@ var PORT = process.env.PORT || 3200;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
+var tableRes = [];
+var waitRes = [];
+
 app.get("/", function(req, res){
 	res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -21,3 +25,24 @@ app.get("/add", function(req, res){
 app.listen(PORT, function(){
 	console.log("App listening on port: " + PORT);
 })
+
+app.post("/api/add", function(req, res){
+	var newRes = req.body;
+
+	console.log(newRes);
+
+	if(tableRes.length <= 5){
+		tableRes.push(newRes);
+		res.json(newRes);
+	}
+
+	if(tableRes >= 6 ){
+		waitRes.push(newRes);
+		res.json(newRes);
+	}
+
+
+
+})
+
+//res.json({object: to send})
